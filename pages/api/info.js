dd552e2b52e9ba749f6dabb988b99e279b60a3eb
Hill Gou <hill.gou@anker.com>
2021-10-28 15:15:12 +0800

    提交到GitHub

---
+++ pages/api/info.js
@@ -0,0 +1,8 @@
+export default (req, res) => {
+    res.status(200).json({ 
+      data: {
+          name: ' hill',
+          Phone: '1234567890'
+      }
+    })
+  }
---
+++ pages/posts/user.js
@@ -0,0 +1,22 @@
+import useSWR from 'swr'
+import axios from 'axios'
+import Layout from '../../components/layout'
+
+// 在客户端进行外部数据请求，使用useSWR hooks
+// const todosEndpoint = "http://localhost:5000/user/info" //   外部服务的api
+const todosEndpoint = "/api/info" // nextjs自带的路由api
+export default function Profile() {
+    const getData = async ()=> {
+        const res = await axios.post(todosEndpoint);
+        return res.data && res.data.data
+    }
+    const { data, error } = useSWR(todosEndpoint, getData, { refreshInterval: 0 })
+    console.log('data', data)
+    if (error) return <div>failed to load</div>
+    if (!data) return <div>loading...</div>
+    return (
+        <Layout>
+            <div>hello {data.name}!</div>
+        </Layout>
+    )
+}
